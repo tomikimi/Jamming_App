65fee4edf9dcb4130520ccc0c354aa7f56fc02fb
Tomikimi <tomikimiogoli@yahoo.com>
2026-03-21 18:24:59 +0100

    CSS Design of PlayList Form

--- src/PlaysList.jsx
+++ src/PlaysList.jsx
@@ -4,7 +4,38 @@ function PlayList() {
   return (
     <>
       <section>
-        <div className={PlayListStyle.playList_container}></div>
+        <div className={PlayListStyle.playList_container}>
+          <div className={PlayListStyle.playlist_form}>
+            <form action="">
+              <div className={PlayListStyle.form_Control}>
+                <label htmlFor="Name" id="Name">
+                  Name
+                </label>
+                <input
+                  type="text"
+                  name="Name"
+                  id="Name"
+                  className={PlayListStyle.form_input}
+                />
+              </div>
+              <div className={PlayListStyle.form_Control}>
+                <label htmlFor="Description" id="Description">
+                  Description
+                </label>
+                <input
+                  type="text"
+                  name="Description"
+                  id="Description"
+                  className={PlayListStyle.form_input}
+                />
+              </div>
+              <div className={PlayListStyle.btn_container}>
+                <button className={PlayListStyle.btn_create}>Create</button>
+              </div>
+            </form>
+          </div>
+          <div></div>
+        </div>
       </section>
     </>
   );
